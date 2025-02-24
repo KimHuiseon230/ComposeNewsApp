eb64bf8828c8f817f0afc729163e2425f17c640b
KimHuiseon <envy089@gmail,com>
2025-02-24 19:57:54 +0900

    [CHORE]:AndroidManifest.xml,build.gradle.kts,libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 }
 
 android {
-    namespace = "com.sesac.newsapipbl"
+    namespace = "com.sesac.composenewsapp"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.sesac.newsapipbl"
+        applicationId = "com.sesac.composenewsapp"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
@@ -55,6 +55,9 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
